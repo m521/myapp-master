d42aa0ca298d8bea295be98e62a131e78c3b6dcb
m521 <mohamedismail.mezhoud@esprit.tn>
2023-05-30 15:23:34 +0100

    lastButNotLeast

---
+++ src/app/Service/registration-form.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+
+const API_URL ="http://localhost:8385/SpringMvc/users/";
+const API_URL_VOITURE ="http://localhost:8385/SpringMvc/";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RegistrationFormService {
+  constructor(private Http:HttpClient) {
+
+  }
+
+  create(user:any): Observable<any>{
+
+    return this.Http.post(API_URL, user);
+
+  }
+}
